4beef6e26779df552a6b8fc515ba8a312c926008
GooseCool1 <asorokovov0@gmail.com>
2026-01-26 07:31:30 +0500

    Python Compatibility

---
+++ __init__.py
@@ -0,0 +1,22 @@
+#  /│  /├  /─  /└  #Fast Copy/Paste
+"""
+QTENSOR Python Compatibility
+
+QTENSOR/
+│ __init__.py
+│ .gitattributes
+│ LICENSE
+│ README.md
+"""
+
+
+__version__ = "1.0.0"
+
+
+#IMPORTS
+...
+
+
+__all__ = [
+    ...
+]
